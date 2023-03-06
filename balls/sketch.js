fedb0c4eb01557951185acb80e7a53f6b325af56
LelFan <fridgepudding@gmail.com>
2023-03-06 15:20:33 -0600

    balls update

--- balls/sketch.js
+++ balls/sketch.js
@@ -4,12 +4,54 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let shapes = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ball(width/2,height/2);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  moveShapes();
+  displayShapes();
+}
+
+function ball(tempX,tempY) {
+  let newball = {
+    x: tempX,
+    y: tempY,
+    dx: random(-5,5),
+    dy: random(-5,5),
+    diameter: random(25,100),
+    theColor: color(random(255),random(255),random(255))
+  };
+  shapes.push(newball);
+}
+
+function moveShapes() {
+  for (let i=0; i<shapes.length; i++) {
+    if (shapes[i].x < 0 || shapes[i].x > width) {
+      shapes[i].dx = shapes[i].dx * -1;
+    }
+    if (shapes[i].y < 0 || shapes[i].y > height) {
+      shapes[i].dy = shapes[i].dy * -1;
+    }
+
+
+    shapes[i].x += shapes[i].dx;
+    shapes[i].y += shapes[i].dy;
+  }
+}
+
+function displayShapes() {
+  for (let i=0; i<shapes.length; i++) {
+    noStroke();
+    fill(shapes[i].theColor);
+    circle(shapes[i].x,shapes[i].y,shapes[i].diameter);
+  }
+}
+
+function mousePressed() {
+  ball(mouseX,mouseY);
 }
