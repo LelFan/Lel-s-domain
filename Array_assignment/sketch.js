d2b602d064262cc65458879e73604b1d676cef68
LelFan <fridgepudding@gmail.com>
2023-03-15 15:18:42 -0600

    pizza

--- Array_assignment/sketch.js
+++ Array_assignment/sketch.js
@@ -4,7 +4,11 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let toppings = ["pepperoni", "mushrooms","peppers","basil","ham"];
+let cheese = ["mozarella","provolone", "cheddar", "parmesan"];
+let bakeTime = [10,12,15,20];
+let orders = []
+let pizz = []
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +16,14 @@ function setup() {
 
 function draw() {
   background(220);
+  createOrder();
+}
+
+createOrder() {
+  let pizza = {
+    cheese: cheese[random(0,4)],
+    topping: toppings[random(0,5)],
+    time: bakeTime[random(0,4)]
+  }
+  orders.push(pizza)
 }
