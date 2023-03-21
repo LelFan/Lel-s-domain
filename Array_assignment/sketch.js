7808a2ae8f5bc4e3ee9442c8056c001a33f6998a
LelFan <fridgepudding@gmail.com>
2023-03-21 02:20:57 -0600

    Update sketch.js

--- Array_assignment/sketch.js
+++ Array_assignment/sketch.js
@@ -4,13 +4,14 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let toppings = ["pepperoni", "mushrooms","peppers","basil","ham"];
-let cheese = ["mozarella","provolone", "cheddar", "blue cheese"];
+let toppings = ["pepperoni","olives","basil","ham"];
+let cheese = ["mozarella","moldy cheese", "cheddar", "blue cheese"];
 let pizzaType = ["thin crust","normal crust","square", "thin square"];
 let orders = [];
 let pizz = [];
 let difficulty = 3;
 let big = 150;
+let basilSize = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -25,7 +26,7 @@ function draw() {
 }
 
 function createOrders() {
-  for (let i=0; i <3; i++) {
+  for (let i=0; i <difficulty; i++) {
 
     let pizza = {
       cheese: cheese[round(random(0,4))],
@@ -41,11 +42,55 @@ function createPizza(){
     if (orders[i].type === "square" || orders[i].type === "thin square") {
       fill(222, 165, 80);
       rect(width/(difficulty + 1) * (i+1), height/2,big,big);
-      
+      drawCheese(i);
     } 
     else {
       fill(222, 165, 80);
       ellipse(width/(difficulty + 1) * (i+1), height/2,big,big);
+      drawCheese(i);
     }
   }
+}
+
+function drawCheese(pizza_number){
+  if (orders[pizza_number].cheese === "mozarella"){
+    fill("white")
+  }
+  else if (orders[pizza_number].cheese === "blue cheese"){
+    fill(173, 219, 237)
+  }
+  else if (orders[pizza_number].cheese === "cheddar"){ 
+    fill("orange")
+  }
+  else {
+    fill(131, 179, 102)
+  }
+
+  if (orders[pizza_number].type === "square" ){
+    rect(width/(difficulty + 1) * (pizza_number+1), height/2,big-30,big-30);
+  }
+  else if (orders[pizza_number].type === "thin square" ){
+    rect(width/(difficulty + 1) * (pizza_number+1), height/2,big-10,big-10);
+  }
+  else if (orders[pizza_number].type === "normal crust" ){
+    ellipse(width/(difficulty + 1) * (pizza_number+1), height/2,big-30,big-30);
+  }
+  else{
+    ellipse(width/(difficulty + 1) * (pizza_number+1), height/2,big-10,big-10);
+  }
+    
+}
+
+function drawToppings(pizza_number){
+  if (orders[pizza_number].toppings === "ham") {
+    fill(166, 108, 133); 
+  }
+  else if (orders[pizza_number].toppings === "olive") {
+    noFill();
+    stroke("black");
+  }
+  else if (orders[pizza_number].toppings === "pepperoni") {
+    fill("red")
+  }
+  ellipse(width/(difficulty + 1) * (pizza_number+1), height/2 +30, )
 }
