cb36ee7a22c32df4e43ea7cf3db56109eff38ef5
LelFan <fridgepudding@gmail.com>
2023-03-22 02:29:59 -0600

    Update sketch.js

--- Array_assignment/sketch.js
+++ Array_assignment/sketch.js
@@ -1,34 +1,49 @@
-// Project Title
-// Your Name
-// Date
+// Pizza guesser
+// Wasi M.
+// March 21st, 2023
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// The entire project has an asdjustable difficulty, allowing for the entire thing to work up to 9. I also used the text() function along with several associated functions to give instructions
+
+//setting up global variables
 let toppings = ["pepperoni","olives","basil","ham"];
 let cheese = ["mozarella","moldy cheese", "cheddar", "blue cheese"];
-let pizzaType = ["thin crust","normal crust","square", "thin square"];
+let pizzaType = ["thin","normal","square", "thin square"];
 let orders = [];
 let pizz = [];
-let difficulty = 5;
+let difficulty = 6;
 let big = 150;
 let basilSize = 0;
+let answer = [];
+let guess = [];
+let x;
+let message;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   noStroke();
   createOrders();
+  createAnswer();
+  textAlign(CENTER);
+  textSize(20);
+
+  //instructing the player on which pizza they're looking for.
+  message = "Which pizza has " + answer[0].type + " crust, " + answer[0].cheese +" cheese and has " + answer[0].topping +"?";
 }
 
 function draw() {
-  background(220);
+  background(220); 
   createPizza();
-  createButton("wasi");
+  //displaying instructions
+  fill("black");
+  text(message, width/2, height/2 + big) 
 }
 
 function createOrders() {
   for (let i=0; i <difficulty; i++) {
 
+    //creating a set of randomized pizzas as many times as the difficulty asks for
     let pizza = {
       cheese: cheese[round(random(0,3.5))],
       topping: toppings[round(random(0,3.5))],
@@ -39,6 +54,7 @@ function createOrders() {
 }
 
 function createPizza(){
+  //drawing all the pizzas
   for (let i=0; i <difficulty; i++) {
     if (orders[i].type === "square" || orders[i].type === "thin square") {
       fill(222, 165, 80);
@@ -56,6 +72,7 @@ function createPizza(){
 }
 
 function drawCheese(pizza_number){
+  //drawing the cheese and using it to also make the crust
   if (orders[pizza_number].cheese === "mozarella"){
     fill("white");
   }
@@ -75,7 +92,7 @@ function drawCheese(pizza_number){
   else if (orders[pizza_number].type === "thin square" ){
     rect(width/(difficulty + 1) * (pizza_number+1), height/2,big-10,big-10);
   }
-  else if (orders[pizza_number].type === "normal crust" ){
+  else if (orders[pizza_number].type === "normal" ){
     ellipse(width/(difficulty + 1) * (pizza_number+1), height/2,big-30,big-30);
   }
   else{
@@ -85,24 +102,52 @@ function drawCheese(pizza_number){
 }
 
 function drawToppings(pizza_number){
+  //drawing toppings
   if (orders[pizza_number].topping === "ham") {
     fill(166, 108, 133); 
   }
   else if (orders[pizza_number].topping === "olives") {
     noFill();
     stroke(51);
+    strokeWeight(4);
   }
   else if (orders[pizza_number].topping === "pepperoni") {
     fill("red");
   }
   else if (orders[pizza_number].topping === "basil") {
     fill("green");
-    basilSize = 5;
+    basilSize = 10;
   }
   ellipse(width/(difficulty + 1) * (pizza_number+1), height/2 +30, 20 - basilSize,20);
   ellipse(width/(difficulty + 1) * (pizza_number+1) +30 , height/2 -30, 20 - basilSize,20);
   ellipse(width/(difficulty + 1) * (pizza_number+1) -30 , height/2 -30, 20 - basilSize,20);
   noStroke();
   basilSize = 0;
+  strokeWeight(1);
 }
 
+function mousePressed() {
+  //allows user to select a pizza 
+  if (mouseY >(height/2 -big/2) && mouseY <(height/2 + big/2) && mouseX < width - width/(difficulty*2) && mouseX >width/(difficulty*2)){
+    let x = Math.floor(mouseX/(width/difficulty));
+    if (guess.length ===0){
+      guess.push(orders[x])
+      checkAnswer();
+    }
+  }
+}
+
+function createAnswer() {
+  //generates an answer based on the random pizzas
+  answer.push(orders[round(random(0,difficulty + 0.5))])
+} 
+
+function checkAnswer() {
+  //checks if the guessed pizza is right
+  if (answer[0] === guess[0]){
+    message = "Good job, refresh to play again."
+  }
+  else {
+    message = "Better luck next time, refresh to play again."
+  }
+}
